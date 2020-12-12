cb86f047e9d044fe60ab1572f85a14c206023f2b
kisukpark <kisukpark@kisukpark-mac.T-mobile.com>
2020-12-13 00:01:34 +0900

    master read argument

--- src/main/scala/sorting/SortingMaster.scala
+++ src/main/scala/sorting/SortingMaster.scala
@@ -1,34 +1,39 @@
 package sorting
 
 import java.net.InetAddress
+
 import com.typesafe.scalalogging.Logger
 import com.sorting.protos.sorting.{GreeterGrpc, HelloReply, HelloRequest}
 import io.grpc.{Server, ServerBuilder}
+import sorting.SortingMaster.host
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object SortingMaster {
-
-  def main(args: Array[String]): Unit = {
-    val server = new SortingMaster(ExecutionContext.global)
-    server.start()
-    server.blockUntilShutdown()
-  }
-
+  private val logger = Logger(classOf[SortingMaster])
   private val host = InetAddress.getLocalHost.getHostAddress
   private val port = 7001
   private val address = s"${host}:${port}"
+
+  var numberOfSlaves = 0;
+
+  def main(args: Array[String]): Unit = {
+    val server = new SortingMaster(ExecutionContext.global, args, logger)
+    server.start()
+    server.blockUntilShutdown()
+  }
 }
 
-class SortingMaster(executionContext: ExecutionContext) { self =>
-  private val logger = Logger(classOf[SortingSlave])
+class SortingMaster(executionContext: ExecutionContext, args: Array[String], logger: Logger) { self =>
   private[this] var server: Server = null
 
   private def start(): Unit = {
     server = ServerBuilder.forPort(SortingMaster.port).addService(GreeterGrpc.bindService(new GreeterImpl, executionContext)).build.start
-    logger.debug("debug")
-    logger.info("info")
-    logger.error("error")
+
+    SortingMaster.numberOfSlaves = args(0).toInt
+    logger.info(s"Master's address ${SortingMaster.address}")
+    logger.info(s"Number of slaves ${SortingMaster.numberOfSlaves}")
+
     sys.addShutdownHook {
       System.err.println("*** shutting down gRPC server since JVM is shutting down")
       self.stop()
